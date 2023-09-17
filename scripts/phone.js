a6864dc1e6f4fcdd3741c2a4b91df4eb34de78a4
bibjaw99 <bibjaw99@gmail.com>
2023-09-17 14:24:05 +0600

    search functionality added

--- scripts/phone.js
+++ scripts/phone.js
@@ -1,97 +1,65 @@
-// search bar
-const searchHandler = (isShowAll) => {
-  loadingAnimation(true);
-  const searchField = document.getElementById("search-field");
-  const searchFieldText = searchField.value;
-  loadPhone(searchFieldText, isShowAll);
-  // searchField.value = "";
+// search button
+const handleSearch = () => {
+  const searchField = document.getElementById("searchbox");
+  const searchText = searchField.value;
+  loadPhone(searchText);
 };
 
-const handleShowAll = () => {
-  searchHandler(true);
-};
-
-const loadPhone = async (searchText, isShowAll) => {
+// the load function will fetch the api
+const loadPhone = async (searchText) => {
   const res = await fetch(
     `https://openapi.programming-hero.com/api/phones?search=${searchText}`,
   );
-  const phoneObj = await res.json();
-  const phones = phoneObj.data;
-  displayPhones(phones, isShowAll);
+  const data = await res.json();
+  const phones = data.data; //  this is where the array of phones is stored. Each of the element in this array is an object
+  displayPhones(phones);
 };
 
-const handleShowDetails = async (slug) => {
-  console.log("kaaj hoitese", slug);
-  const res = await fetch(
-    `https://openapi.programming-hero.com/api/phone/${slug}`,
-  );
-  const details = await res.json();
-  console.log(details);
-};
+// phone container (parent div that will contain the div for each phone created dinamically)
+const phoneContainer = document.getElementById("phones-container");
 
-// the function will control the loading Animation
-const loadingAnimation = (isLoading) => {
-  const loadingSpinner = document.getElementById("loading-spinner");
-  if (isLoading) {
-    loadingSpinner.classList.remove("hidden");
-  } else {
-    loadingSpinner.classList.add("hidden");
-  }
-};
-
-// This function display the phones which are loaded
-const displayPhones = (phoneArray, isShowAll) => {
-  const phoneContainer = document.getElementById("phone-container");
+// displayPhones will take each element from the array and execute the operations to showcase each phones
+const displayPhones = (phonesArray) => {
+  // by default we want the parent div (phones-container) to be empty
   phoneContainer.textContent = "";
-
-  const showAllContainer = document.getElementById("show-all-container");
-  if (phoneArray.length > 12 && !isShowAll) {
-    showAllContainer.classList.remove("hidden");
-  } else {
-    showAllContainer.classList.add("hidden", true);
-  }
-  // console.log("is show all: ", isShowAll);
-
-  if (!isShowAll) {
-    phoneArray = phoneArray.slice(0, 12);
-  } else {
-    phoneArray = phoneArray.slice(0, -1);
-  }
-
-  phoneArray.forEach((phone) => {
+  phonesArray.forEach((phone) => {
     console.log(phone);
-    const eachProduct = document.createElement("div");
-    eachProduct.classList = "card bg-white shadow-xl p-5";
-    eachProduct.innerHTML = `
 
+    // step:1 create a div
+    const phoneCard = document.createElement("div"); // protiti phone er jonno ekta kore div  nilam
+
+    // setp:2 add list of classes
+    phoneCard.classList = `card bg-white shadow-xl p-5`;
+
+    // protiti phone er html property
+    // setp:3 set inner htmls
+    phoneCard.innerHTML = `
       <!-- img -->
-      <figure class="mt-5";>
-        <img src=${phone.image} alt="Shoes" />
+      <figure>
+      <img src=${phone.image} alt="Shoes" />
       </figure>
 
       <!-- text part -->
-      <div class="card-body flex flex-col gap-5">
-      <!-- title -->
+      <div class="card-body flex flex-col gap-6">
+        <!-- title -->
         <h2 class="text-center text-2xl font-bold">${phone.phone_name}</h2>
 
-      <!-- descriptions -->
-        <p class="text-center">
-        There are many variations of passages of available, but the majority have suffered
-        </p>
+        <!-- descriptions -->
+        <p class="text-center">If a dog chews shoes whose shoes does he choose?</p>
 
         <h3 class="text-center text-2xl font-bold">$99</h3>
         <!-- button action -->
         <div class="card-actions justify-center">
-          <button
-            onclick="handleShowDetails('${phone.slug}')"
-            class="btn btn-primary text-white bg-blue-600 border-none text-center font-semibold px-6 py-3"
-          >
-            show more
-          </button>
+        <button
+        class="btn btn-primary text-white bg-blue-600 border-none text-center font-semibold px-6 py-3"
+        >
+        show more
+        </button>
         </div>
       </div>
     `;
-    phoneContainer.appendChild(eachProduct);
+
+    // setp:4 append the product in a parent container
+    phoneContainer.appendChild(phoneCard);
   });
-  loadingAnimation(false); // disable the animation after displaying the  phones
 };
